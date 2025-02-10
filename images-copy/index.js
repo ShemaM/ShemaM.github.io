7a3ffecd67787116b34878a31df8be06272f1063
Shema Manasseh <shemamanase992@gmail.com>
2025-02-11 02:57:34 +0300

    home and impact sections

--- images-copy/index.js
+++
@@ -1,4 +0,0 @@
-export const background = require("./aboutus.jpg");
-export const backgroundBlur = require("./aboutus-blur.jpg");
-export const collab = require("./collab.jpg");
-export const collabBlur = require("./collab-blur.jpg");
